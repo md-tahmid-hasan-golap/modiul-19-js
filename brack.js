b71f3f62fe96841bdda3dfe69721016d91a8b408
golap-raj <golapraj47@gmail.com>
2025-02-07 11:52:25 -0600

    java add

---
+++ brack.js
@@ -0,0 +1,6 @@
+for(let i = 1; i <= 30; i++){
+    console.log(i);
+    if(i >= 20){
+        break;
+    }
+}
--- evens.js
+++ evens.js
@@ -19,8 +19,25 @@
 //     i=i+2;
 // }
 
-for(let i = 1; i <= 30; i++){
-    if(i % 5 === 0){
-        console.log(i);
-    }
+// for(let i = 0; i <= 30; i = i+5){
+//     // if(i % 5 === 0){
+//     //     console.log(i);
+//     console.log(i)
+//     // }
+// }
+
+// for(let i = 0; i <=30; i++){
+//     if(i % 2 === 1 || i % 5 === 0){
+//         console.log(i)
+//     }
+// }
+let total = 0;
+for(let i = 1; i<= 20; i++)
+    if(i % 3 === 0) {
+   
+    console.log(i);
+    total = total + i;
+   
+
 }
+console.log(total);
