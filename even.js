078aa2f57af6bc39bf830036137bebc0ef51b614
golap-raj <golapraj47@gmail.com>
2025-02-07 10:49:23 -0600

    add java

--- even.js
+++ even.js
@@ -1,7 +1,67 @@
-let number = 1;
-while(number <= 10){
-    console.log(number);
-    if(number % 2 === 0);
-    console.log("even number :", number);
-    number++;
+// let number = 1;
+// while(number <= 10){
+//     console.log(number);
+//     if(number % 2 === 0);
+//     console.log("even number :", number);
+//     number++;
+// }
+// let sum = 0;
+
+// for(let i = 200; i <= 250; i++){
+//     sum = sum + i;
+//     console.log(i)
+
+    
+// }
+// console.log("sum number :", sum)
+
+// let i = 200;
+// let sum = 0;
+// while(i <= 300){
+//     console.log(i);
+//     sum = sum + i;
+//     console.log(sum)
+//     i++;
+// }
+
+// for(let i = 10; i >= 0; i--){
+//     console.log(i)
+// }
+
+// let i = 10;
+// while(i >= 0){
+//     console.log(i);
+//     i--;
+// }
+
+// let number = 0;
+
+// while(number <= 50){
+//     console.log(number)
+//     let jor = number % 2 === 0;
+//     console.log(jor);
+//     number++;
+// }
+
+
+// for(let i = 1; i <= 20; i++){
+//     if(i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+
+// for(let i = 1; i <= 50; i++){
+//     if(i % 3 === 0 && i % 5 === 0){
+//         console.log(i)
+//     }
+// }
+
+let total = 0;
+for(let i = 0; i<= 20; i++){
+    if(i % 3 === 0){
+        console.log(i);
+        total = total + i;
+        console.log("total :", total);
+    }
+   
 }
---
+++ evens.js
@@ -0,0 +1,26 @@
+// for(let i = 1; i <= 20; i++){
+//     if(i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+
+// for(let i =0; i <= 20; i++){
+//     if(i % 2 === 1){
+//         console.log(i)
+//     }
+// }
+
+// for(let i = 0; i <= 20; i=i+2){
+//     console.log(i)
+// }
+// let i = 0;
+// while(i <= 20){
+//     console.log(i);
+//     i=i+2;
+// }
+
+for(let i = 1; i <= 30; i++){
+    if(i % 5 === 0){
+        console.log(i);
+    }
+}
