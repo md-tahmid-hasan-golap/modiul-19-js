378894314eece3c0ab7ba7605fb9c7213ca8e637
golap-raj <golapraj47@gmail.com>
2025-02-07 12:36:15 -0600

    prestice java

---
+++ prestice.js
@@ -0,0 +1,77 @@
+// let i = "আমি আগামী ৬০ দিনের জন্য প্রতিদিন কমপক্ষে ৬ ঘণ্টা বিনিয়োগ করবো!";
+// while(i < 60);{
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 61;
+// while(i <= 72){
+//     console.log(i)
+//     i=i+2
+// }
+// let i = 78;
+// while(i <= 98){
+//     console.log(i)
+//     i=i+2
+// }
+
+
+// let i = 81;
+// let total = 0;
+// while(i <= 131){
+//     if(i % 2 === 1){
+//         console.log(i);
+//         total = total + i;
+//         i++;
+//     }
+// }
+
+// console.log("total :", total)
+
+// let total = 0;
+// for(let i = 81; i<= 131; i++){
+//     if(i % 2 === 1){
+//         console.log(i);
+//         total = total + i;
+//     }
+// }
+// console.log(total);
+// let total = 0;
+// for(let i = 206; i<= 311; i++){
+//     if(i % 2 === 0){
+//         console.log(i);
+//         total = total + i;
+//     }
+// }
+// console.log(total);
+
+// for(let i = 21; i >= 15; i--){
+//     console.log(i)
+//     if(i > 15)
+// }
+
+// let i = 1;
+// while(i <= 40){
+//     i++;
+//     if(i % 2 === 1){
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+// for(let i = 55; i <= 85; i++ ){
+//     if(i % 5 === 0){
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+let i = 0;
+while(i <= 30){
+    i++;
+    if(i > 20){
+        break;
+        
+    }
+    console.log(i);
+}
